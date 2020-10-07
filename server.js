58a06af31becdd439def0512b8fbcb2d2c6f6560
MoatazDj <djebalimootez1@gmail.com>
2020-10-07 16:41:12 +0100

    initializing the server

--- server.js
+++ server.js
@@ -1,3 +1,23 @@
 const express = require('express')
 const morgan = require('morgan')
 const bodyparser = require('body-parser')
+const cors = require('cors')
+
+
+require('dotenv').config({
+    path:'./server/config/config.env'
+})
+const app = express()
+
+// if (process.env.NODE_ENV === 'development') {
+//     app.use(cors({
+//         origin: process.env.CLIENT_URL
+//     }))
+//     app.use(morgan('dev'))
+// }
+
+const PORT = process.env.PORT
+
+app.listen(PORT,()=>{
+    console.log(`App is Fucking working! ${PORT}`)
+})
